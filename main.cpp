80c7a72926674253ca228d03f451e440f707329d
s-Danielle <sdanielle@campus.technion.ac.il>
2024-11-23 21:07:03 +0200

    work in progress

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include "tokens.hpp"
 #include "output.hpp"
+#include <cstdio>
+#include <string>
+
+void handleString(std::string str, int len){
+	
+}
 
 int main() {
     enum tokentype token;
 
     // read tokens until the end of file is reached
+    std::string temp;
     while ((token = static_cast<tokentype>(yylex()))) {
         switch (token) {
 		case UKCHAR:
-			output::errorUnknownChar(*yytext);
+			output::errorUnknownChar(yytext[yyleng-1]);
 			break;
 		case UCSTR:
 			output::errorUnclosedString();
+			break;
 		case UDESC:
 			output::errorUndefinedEscape(yytext); //check this i might be stupid
-		deafault:
+			break;
+		case STRING:
+			temp = yytext;
+			handleString(temp, yyleng);
+			output::printToken(yylineno,token,temp.c_str());
+			break;
+		default:
 			output::printToken(yylineno,token,yytext);
 			break;
 	 }
--- scanner.lex
+++ scanner.lex
@@ -11,7 +11,7 @@
 digit	([0-9])
 letter	([a-zA-Z])
 whitespace	([\t\n\r ])
-
+printable              ((x[2-6][0-9A-Fa-f])|x7[0-9A-Ea-e])
 
 
 %%
@@ -45,5 +45,10 @@ continue      return CONTINUE;
 {letter}[a-zA-Z0-9]*		 return ID;
 ([1-9]+{digit}*b)|0b		return NUM_B;
 ([1-9]+{digit}*)|0     	 return NUM;
+\"([^\\\"\n\r]|\\[\\nrt0"\\]|\\x[0-9a-fA-F]{2})*\"	 return STRING;
+\"([^\\\"\n\r]|\\[\\nrt0"\\]|\\x[0-9a-fA-F]{2})*  	return UCSTR;
+\"([^\\\"\n\r]|\\[\\nrt0"\\]|\\x[0-9a-fA-F]{2})*\\[^\\nrt0"\\x]		return UKCHAR;
+\"([^\\\"\n\r]|\\[\\nrt0"\\]|\\x[0-9a-fA-F]{2})*\\[^a-fA-F0-9]{2}.*\"  	return UDESC;
 {whitespace}+		;
+
 .		return ERROR;
