4e94962771e20147a19944aa5e1249e745c7ef82
s-Danielle <sdanielle@campus.technion.ac.il>
2024-12-01 18:31:27 +0200

    workinggggg

--- main.cpp
+++ main.cpp
@@ -2,16 +2,14 @@
 #include "output.hpp"
 #include <cstdio>
 #include <string>
- 
-	
-void parseIllegalSeq(std::string &src, int index){
-    if (src[index+1]!='x'){
+void parseIllegalSeq(std::string &src, int index) {
+    if (src[index + 1] != 'x') {
         src = src.substr(index + 1, 1);
         return;
     }
-    char next=src[index+2], nextx2=src[index+3]?src[index+3]:'\0';
-    if (((next>='a'&&next<='z')||(next>='A'&&next<='Z'))||(next>='0'&&next<='9')){
-        if (((nextx2>='a'&&nextx2<='z')||(nextx2>='A'&&nextx2<='Z'))||(nextx2>='0'&&nextx2<='9')){
+    char next = src[index + 2], nextx2 = src[index + 3] ? src[index + 3] : '\0';
+    if (next != '\"') {
+        if (nextx2 != '\"') {
             src = src.substr(index + 1, 3);
             return;
         }
@@ -21,75 +19,77 @@ void parseIllegalSeq(std::string &src, int index){
     src = src.substr(index + 1, 1);
 }
 
-//on success return -1, on failure returns index of illegal seq in src 
-int parse_string(std::string &source, std::string &result){
-	result="";
-	char current_char, next_char, hex[3];
-	int ascii, index=0;
-	while(source[index]){
-		current_char=source[index];
-		switch (current_char) {
-			case '\"':
-				//ignore in beginning and end " ", if we didn't handle input case of " hi \" this is where we check end for uncstr
-				index++;
-				continue;
-			case '\\':
-				if (index + 1 >= source.length()) {
-					return index;
-				}
-				next_char = source[index + 1];
-				if (!next_char) {
-					return index;
-					// we shouldn't get here but if we do its uncstr
-				 }
-				switch (next_char) {
-					case '\\':
-						result += '\\';
-						break;
-					case 'n':
-						result += '\n';
-						break;
-					case 't':
-						result += '\t';
-						break;
-					case 'r':
-						result += '\r';
-						break;
-					 case '\"':
-						result += '\"';
-						break;
-					case '0':
-						result+'\0';
-						break;
-					case 'x':
-						if (!source[index + 2] || !source[index + 3]) {
-							return index + 1;
-						}
-						hex[0] = source[index + 2];
-						hex[1] = source[index + 3];
-						hex[2] = '\0';
-						ascii = strtol(hex, nullptr, 16);
-						//todo add illegal seq
-						 if (ascii<0x20 || ascii>0x7e){
-							 return index;
-						 }
-						 result += (char) ascii;
-						 index+=2;
-						 break;
-					 default:
-			 			return index + 1;
-					 }
-				index += 2;
-	 			continue;
-	 		default: 
-				result+=current_char;
-				index++;
-				continue;
-		}
-	}
-	return -1;
-}
 
+int parse_string(std::string &source, std::string &result){
+    result="";
+    int checkNullindex=-1;//this is a shitty solution but in tired
+    char current_char, next_char, hex[3];
+    int ascii, index=0;
+    while(source[index]){
+        current_char=source[index];
+        switch (current_char) {
+            case '\"':
+                index++;
+                continue;
+            case '\\':
+                if (index + 1 >= source.length()) {
+                    return index;
+                }
+                next_char = source[index + 1];
+                if (!next_char) {
+                    return index;
+                }
+                switch (next_char) {
+                    case '\\':
+                        result += '\\';
+                        break;
+                    case 'n':
+                        result += '\n';
+                        break;
+                    case 't':
+                        result += '\t';
+                        break;
+                    case 'r':
+                        result += '\r';
+                        break;
+                    case '\"':
+                        result += '\"';
+                        break;
+                    case '0':
+			if (checkNullindex==-1){
+			checkNullindex=index;}
+                        break;
+                    case 'x':
+                        if (!source[index + 2] || !source[index + 3] ||source[index + 2]=='\"'||source[index + 3]=='\"') {
+                            return index;
+                        }
+                        hex[0] = source[index + 2];
+                        hex[1] = source[index + 3];
+                        hex[2] = '\0';
+                        ascii = strtol(hex, nullptr, 16);
+                        if (ascii<0x20 || ascii>0x7e){
+                            if (!((ascii==0x09 ||ascii==0x0d) ||  ascii==0x0a)){
+                                return index;}
+                        }
+                        result += (char) ascii;
+                        index+=2;
+                        break;
+                    default:
+                        return index + 1;
+                }
+                index += 2;
+                continue;
+            default:
+                result+=current_char;
+                index++;
+                continue;
+        }
+    }
+    if (checkNullindex!=-1){
+	result=result.substr(0,checkNullindex-1);}
+    return -1;
+}
+ 
 int main() {
     enum tokentype token;
     std::string parsingStr="", result="";
