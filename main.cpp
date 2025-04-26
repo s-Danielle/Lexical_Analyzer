f728857b64ca36b10ddb94a7385ac4edb8e5539d
Zohar <Zshtamberg@campus.technion.ac.il>
2025-04-26 19:39:49 +0300

    Enhance string handling: add functions to remove quotes and replace escape sequences

--- main.cpp
+++ main.cpp
@@ -2,14 +2,56 @@
 #include "output.hpp"
 #include <cstdio>
 #include <string>
+#include <vector>
+#include <cassert>
+
+#include <iostream>
 
 
+std::string& removeQuotes(std::string& input) {
+    //assert(*input.begin() == *input.rbegin() == '\"');
+    if (*input.rbegin() == '\"') { input.pop_back(); }
+    if (*input.begin() == '\"') { input.erase(input.begin()); }
+    return input;
+}
+
+std::string replaceEscSequences(const std::string& input) {
+    //there is no error handling here because i count of the lexer to do this work for me.
+    //assuming is correct.
+    std::string result;
+    result.reserve(input.length());
+
+    for (size_t i = 0; i < input.length(); ++i) {
+        if (input[i] == '\\') {
+            assert(i + 1 < input.length());
+            switch (input[i + 1]) {
+            case 'n': result.push_back('\n'); i++; break;
+            case 'r': result.push_back('\r'); i++; break;
+            case 't': result.push_back('\t'); i++; break;
+            case '\\': result.push_back('\\'); i++; break;
+            case '"': result.push_back('"'); i++; break;
+            case '0': return result;
+            case 'x':
+                assert(i + 3 < input.length());
+                std::string hex = input.substr(i + 2, 2); // \xdd, need to skip both 'x' and '\'
+                i += 3; // the loops adds one more anyway!
+                char c = static_cast<char>(std::stoi(hex, nullptr, 16)); // turns out stoi knows how to do this
+                result.push_back(c);
+            }
+        }
+        else {
+            result.push_back(input[i]);
+        }
+    }
+    return result;
+}
 int main() {
     enum tokentype token;
-    std::string parsingStr = "", result = "";
-    int check;
+    std::string parsingStr;
     // read tokens until the end of file is reached
     while ((token = static_cast<tokentype>(yylex()))) {
+        auto seq = &yytext[yyleng - 1];
+        char* buf;
         switch (token) {
         case UKCHAR:
             output::errorUnknownChar(yytext[yyleng - 1]);
@@ -18,11 +60,15 @@ int main() {
             output::errorUnclosedString();
             break;
         case UDESC:
-            output::errorUndefinedEscape(&yytext[yyleng - 1]); //check this i might be stupid
+            for (; *seq != '\\'; seq--);
+            output::errorUndefinedEscape(seq + 1); //check this i might be stupid
             break;
         case STRING:
-            
-            output::printToken(yylineno, token, result.c_str());
+            parsingStr = std::string(yytext);
+            removeQuotes(parsingStr);
+            parsingStr = replaceEscSequences(parsingStr);
+            // std::cout << parsingStr << '\n';
+            output::printToken(yylineno, token, parsingStr.c_str());
             break;
         default:
             output::printToken(yylineno, token, yytext);
--- scanner.lex
+++ scanner.lex
@@ -13,7 +13,7 @@
 digit	([0-9])
 letter	([a-zA-Z])
 whitespace	([\t\n\r ])
-printableCharHex		   ([2-6][0-9a-fA-F]|[2-6][a-fA-F]|7[0-9a-eA-E])
+printableCharHex		   ([2-6][0-9a-fA-F]|7[0-9a-eA-E]|0[9AaDd])
 legalEsc           (\\[\\ntr\"0]|\\x{printableCharHex})
 simpleStrChar           ([ !#-\[\]-~	])
 legalStrContent			({simpleStrChar}|{legalEsc})*
@@ -53,8 +53,8 @@ continue      return CONTINUE;
 ([1-9]+{digit}*b)|0b		return NUM_B;
 ([1-9]+{digit}*)|0     	 return NUM;
 \"{legalStrContent}\"			 return STRING;
-\"{legalStrContent}\n			 return UCSTR;
-\"{legalStrContent}\\x[^\"]{0,2}         return UDESC;
+\"{legalStrContent}			 return UCSTR;
+\"{legalStrContent}\\x[^\"\n]{0,2}         return UDESC;
 \"{legalStrContent}\\[^\\ntr\"0x]	         return UDESC;
 
 {whitespace}+		;
